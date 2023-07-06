c20e21eb26eae9f55912a36a3aa837abf184ec0c
smartdevilmind <121383527+smartdevilmind@users.noreply.github.com>
2023-07-06 09:29:51 +0530

    remove some improper directives from setting

--- igasolutions/settings.py
+++ igasolutions/settings.py
@@ -32,9 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material',
-    'material.admin',
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
